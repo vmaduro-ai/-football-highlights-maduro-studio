b2a600ad77e35647bb144d48b4fa6c76393b0d79
vmaduro-ai <vmaduro@gmail.com>
2026-09-13 00:15:56 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE='fhms-v5';
+const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{
+    const copy=x.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return x;
+  }).catch(()=>caches.match('./index.html'))));
+});
