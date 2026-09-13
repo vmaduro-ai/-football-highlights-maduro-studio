cb929273c67fc7a47b7bf005b904c2f82fec769b
vmaduro-ai <vmaduro@gmail.com>
2026-09-13 18:06:28 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,61 +1,13 @@
-const CACHE = 'fhms-stable-v1';
-const APP = ['./index.html', './manifest.json', './icon.svg'];
+self.addEventListener("install", e => self.skipWaiting());
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then(cache => cache.addAll(APP))
-      .then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
+self.addEventListener("activate", e => {
+  e.waitUntil(
     caches.keys()
-      .then(keys =>
-        Promise.all(
-          keys
-            .filter(k => k !== CACHE)
-            .map(k => caches.delete(k))
-        )
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.matchAll())
+      .then(clients =>
+        Promise.all(clients.map(client => client.navigate(client.url)))
       )
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', event => {
-  const req = event.request;
-
-  if (req.method !== 'GET') return;
-
-  if (req.mode === 'navigate' || req.destination === 'document') {
-    event.respondWith(
-      fetch(req)
-        .then(res => {
-          const copy = res.clone();
-          caches.open(CACHE)
-            .then(cache => cache.put('./index.html', copy));
-          return res;
-        })
-        .catch(() => caches.match('./index.html'))
-    );
-    return;
-  }
-
-  event.respondWith(
-    caches.match(req).then(cached =>
-      cached ||
-      fetch(req).then(res => {
-        if (
-          res.ok &&
-          new URL(req.url).origin === location.origin
-        ) {
-          const copy = res.clone();
-          caches.open(CACHE)
-            .then(cache => cache.put(req, copy));
-        }
-        return res;
-      })
-    )
   );
 });
